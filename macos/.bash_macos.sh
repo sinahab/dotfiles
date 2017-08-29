6c780287417a964784a6911862cabf1d75c6899e
Sina Habibian <sinahab@gmail.com>
2017-08-28 17:36:13 -0700

    Add macos specific bash script (for completeness)

---
+++ macos/.bash_macos.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+export PATH="$HOME/anaconda2/bin:$PATH"
+export PATH="$HOME/anaconda3/bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
+source $HOME/.rvm/scripts/rvm
